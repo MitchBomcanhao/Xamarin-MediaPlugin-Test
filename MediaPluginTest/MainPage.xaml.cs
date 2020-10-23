85ca793f7be4646e50d0a63c612f9e713be360e7
MD\roman <roman.trojan@msgis.com>
2020-10-23 15:35:28 +0200

    rt: Test MediaPlugin

---
+++ MediaPluginTest/MainPage.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
+namespace MediaPluginTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            if (!CrossMedia.IsSupported)
+                DisplayAlert("Not supported", "The plugin CrossMedia is not supported!", "OK");
+
+            DisplayAlert("--- Initialize ---", "The plugin CrossMedia has beenn started!", "OK");
+
+            takePhoto.Clicked += async (sender, args) =>
+            {
+                if (!await CrossMedia.Current.Initialize())
+                    await DisplayAlert("Not initialized", "The plugin CrossMedia is not initialized!", "OK");
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Test",
+                    SaveToAlbum = true,
+                    CompressionQuality = 75,
+                    CustomPhotoSize = 50,
+                    PhotoSize = PhotoSize.MaxWidthHeight,
+                    MaxWidthHeight = 2000,
+                    DefaultCamera = CameraDevice.Front
+                });
+
+                if (file == null)
+                    return;
+
+                await DisplayAlert("File Location", file.Path, "OK");
+
+                image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+            };
+
+            pickPhoto.Clicked += async (sender, args) =>
+            {
+                if (!await CrossMedia.Current.Initialize())
+                    await DisplayAlert("Not initialized", "The plugin CrossMedia is not initialized!", "OK");
+
+                if (!CrossMedia.Current.IsPickPhotoSupported)
+                {
+                    await DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
+                    return;
+                }
+                var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+
+                });
+
+
+                if (file == null)
+                    return;
+
+                image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+            };
+
+            takeVideo.Clicked += async (sender, args) =>
+            {
+                if (!await CrossMedia.Current.Initialize())
+                    await DisplayAlert("Not initialized", "The plugin CrossMedia is not initialized!", "OK");
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakeVideoSupported)
+                {
+                    await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakeVideoAsync(new Plugin.Media.Abstractions.StoreVideoOptions
+                {
+                    Name = "video.mp4",
+                    Directory = "DefaultVideos",
+                });
+
+                if (file == null)
+                    return;
+
+                await DisplayAlert("Video Recorded", "Location: " + file.Path, "OK");
+
+                file.Dispose();
+            };
+
+            pickVideo.Clicked += async (sender, args) =>
+            {
+                if (!await CrossMedia.Current.Initialize())
+                    await DisplayAlert("Not initialized", "The plugin CrossMedia is not initialized!", "OK");
+
+                if (!CrossMedia.Current.IsPickVideoSupported)
+                {
+                    await DisplayAlert("Videos Not Supported", ":( Permission not granted to videos.", "OK");
+                    return;
+                }
+                var file = await CrossMedia.Current.PickVideoAsync();
+
+                if (file == null)
+                    return;
+
+                await DisplayAlert("Video Selected", "Location: " + file.Path, "OK");
+                file.Dispose();
+            };
+        }
+    }
+}
